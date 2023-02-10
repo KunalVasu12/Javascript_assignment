9580cbf7f9b55ba5c2d3c5d00479e7933e0aea02
Kunal Vasu <124379675+KunalVasu12@users.noreply.github.com>
2023-02-10 01:33:09 -0800

    assignment3

---
+++ Javascript all assignment/assignment3/app.js
@@ -0,0 +1,331 @@
+    
+    //   Assignment=03
+//       Chapter=05
+//     Math Expressions
+
+//       Question=01
+document.write('<h3> Chapter=05 </h3>');
+document.write('<h3> "Question # 01 Addition " </h3>');
+var num1=3;
+var num2=5;
+var total=num1+num2;
+document.write("Sum of 3 and 5 is "+ total);
+
+//       Question=02
+document.write('<h3> "Question # 02 subtraction " </h3>');
+var num1=3;
+var num2=5;
+var total=num1-num2;
+document.write("Subtraction of 3 and 5 is "+ total);
+
+document.write('<h3> "Question # 02 multiplication " </h3>');
+var num1=3;
+var num2=5;
+var total=num1*num2;
+document.write("MUltiplicatiuon of 3 and 5 is "+ total);
+
+document.write('<h3> "Question # 02 Division " </h3>');
+var num1=3;
+var num2=5;
+var total=num1/num2;
+document.write("Division of 3 and 5 is "+ total);
+
+document.write('<h3> "Question # 02 Modulus " </h3>')
+var num1=3;
+var num2=5;
+var total=num1%num2;
+document.write("MOdulus of 3 and 5 is "+ total);
+
+//       Question=03
+document.write('<h3> "Question#03 (A) " </h3>')
+   
+var num=
+document.write( "Value after variable declaration is" + " " + num)
+
+document.write('<h3> "Question#03 (B) " </h3>')
+var num=(5)
+document.write( "Intial value:" + num)
+
+document.write('<h3> "Question#03 (C) " </h3>')
+var num=(5)
+document.write("Value after increment is:" + " " + ++ num)
+
+document.write('<h3> "Question#03 (D) " </h3>')
+var num=(4)
+document.write("Value after increment is:" + " " + ++ num)
+
+document.write('<h3> "Question#03 E(A) " </h3>')
+var numa=(5)
+var numb=(8)
+var numc = numa + 8
+document.write( "Value after 2 Variable Addtion is:" + " " + numc)
+
+
+document.write('<h3> "Question#03 E(B) " </h3>')
+var num=(4)
+var numa=(5)
+var numc = num + numa
+document.write("Value after increment is:" + " " + ++ numc)
+
+
+document.write('<h3> "Question#03 (F) " </h3>')
+var num=(5)
+var numa = ++num
+document.write("Value after increment is:" + " " + numa)
+
+document.write('<h3> "Question#03 (G) " </h3>')
+var num=(6)
+var numa = num
+document.write("Value 6+1 after increment is:" + " " + ++num)
+
+document.write('<h3> "Question#03 (H) " </h3>')
+var a= (6)
+var b= (6)
+var c= a+b 
+document.write("Value after increment is 6+6 =" + ++c )
+
+
+document.write('<h3> "Question#03 (I) " </h3>')
+var num=(6)
+var numa = --num
+document.write("Decrement the variable. 6-1 = " + numa)
+
+
+
+document.write('<h3> "Question#03 (J) " </h3>')
+var a= (19)
+var b= (6)
+var c= a-b 
+document.write("Value after Decrement is 19-6 =" + --c )
+
+document.write('<h3> "Question#03 (K) " </h3>')
+var num=(10)
+var numa = num / 3
+document.write("After 10 dividing the variable's value by 3:" + " " + "=" + " " + numa)
+
+document.write('<h3> "Question#03 (L) " </h3>')
+var a=(12)
+var b=(3)
+var c= a%b
+document.write("After 12 Modulus the variable's value by 3:"  + " " + "=" + " " + c)
+
+
+       //       Question=04
+
+ document.write('<h3> "Question#04 " </h3>');
+ document.write("<h5>Cost of one movie ticket is 600 PKR. Write a script to store <br> ticket price in a variable & calculate the cost of buying 5 tickets <br> to a movie.?</h5>");
+
+ document.write('<h3> "Example Output" </h3>');
+ var ticket =(600);
+ var buying =(5);
+ var total = ticket * buying;
+ document.write("Total cost to buy 5 tickets to a movie is " + " " + total + " " + "PKR.");
+
+
+ //       Question=05
+ document.write('<h3> "Question#05 " </h3>');
+ document.write('<h3> Table of 7 </h3>');
+   var aj = 1;
+   var bi = 2;
+   var ch = 3;
+   var dg = 4;
+   var ef = 5;
+   var fe = 6;
+   var gd = 7;
+   var hc = 8;
+   var ib = 9;
+   var ja = 10;
+
+    var ta = aj * gd;  
+    document.write("1x7=" + ta + "<br>");
+
+    var tb = bi * gd;
+    document.write("2x7=" + tb + "<br>");
+
+    var tc = ch * gd; 
+    document.write("3x7=" + tc + "<br>");
+
+    var td = dg * gd;
+    document.write("4x7=" + td + "<br>");
+
+    var te =  ef * gd;
+    document.write("5x7=" + te + "<br>");
+
+    var tf = fe * gd;
+    document.write("6x7=" + tf + "<br>");
+
+    var tg = gd * gd;
+    document.write("7x7=" + tg + "<br>");
+
+    var th = hc * gd;
+    document.write("8x7=" + th + "<br>");
+    
+    var ti = ib * gd;
+    document.write("9x7=" + ti + "<br>");
+
+    var tj = ja * gd;
+    document.write("10x7=" + tj + "<br>");
+
+
+    //       Question=06
+    document.write('<h3> "Question#06 " </h3>');
+    var Celsius = 22;
+    var COnvert_to_fehranheit = (27 * 9 / 5) + 32;
+    var Fehranheit = 72;
+    var COnvert_to_Celsius = (72 - 32) * 5 /9;
+    document.write(Celsius+"<sup>0</sup>C" + "" +"is"+ "" +COnvert_to_fehranheit +"<sup>0</sup>F <br>");
+    document.write(Fehranheit+"<sup>0</sup>F" + "" +"is"+ "" +COnvert_to_Celsius +"<sup>0</sup>C <br>");
+    
+    //      Question=08
+    document.write('<h3> "Question#08 " </h3>');
+    var totalMarks = 980;
+     var marksObtained = 804;
+     var percentage = (marksObtained / totalMarks) * 100;
+     document.write("TOtal Marks: " + totalMarks +"<br>");
+     document.write("Marks obtained: " + marksObtained +"<br>");
+     document.write("Percentage: " + percentage +"<br>");
+  
+     //      Question=09
+     document.write('<h3> "Question#09 " </h3>');
+     var usd = 10;
+     var sar = 25;
+     var pkr = (usd * 104.80) + (sar * 28);
+     document.write("<h1>Currency in PKR</h1>"+"<br>");
+     document.write("The Total Currency in PKR:" + pkr +"<br>");
+  
+     //      Question=10
+     document.write('<h3> "Question#10 " </h3>');
+     var num = 10;
+     var result = (num + 5) * 10 / 2;
+     document.write("a) Add 5" + "<br>");
+     document.write("b) Multiply by 10" + "<br>");
+     document.write("c) Divide the result by 2" + "<br>");
+     document.write("Perform all calculations in a single expression " + result +"<br>");
+    
+     //      Question=11
+     document.write('<h3> "Question#11 " </h3>');
+     var currentYear = 2023;
+     var birthYear = 2003;
+     var age = currentYear - birthYear;
+     document.write("<h2> Age Calculator </h2>");
+     document.write("Current year: "+ currentYear + "<br>");
+     document.write("Birth year: "+ birthYear+"<br>");
+     document.write("My Age is: "+ age+"<br>");
+ 
+     //       Question=12
+     document.write('<h3> "Question#12 " </h3>');
+     var radius = 20;
+     var pi = 3.142;
+
+     var circumference = 2 * pi * radius;
+     document.write("Radius of a circle is: "+radius+"<br>");
+     document.write("The circumference is "+circumference+"<br>");
+
+     var area = pi * radius * radius;
+     document.write("The area is "+area+"<br>");
+
+     //       Question=13
+     document.write('<h3> "Question#13 " </h3>');
+     
+     var favoriteSnack = "chocolate bars";
+     var currentAge = 15;
+     var maximumAge = 65;
+     var amountPerDay = 3;
+     var totalAmount = (maximumAge - currentAge) * 365 * amountPerDay;
+     document.write('<h1>The Lifetime Supply Calculator </h1>');     
+     document.write("FavoriteSnack: " + favoriteSnack+"<br>");
+     document.write("Current age: " + currentAge+"<br>");
+     document.write("Estimated Maximum Age: " + maximumAge+"<br>");  
+     document.write("Amount Of snacks Per day: " + amountPerDay+"<br>");
+     document.write("You will need " + totalAmount+ favoriteSnack +"to last you until the ripe old age of " + maximumAge);
+
+
+        //Chapter=6-9
+     //Math Expressions
+     document.write("<h3> Chapter=6-9 </h3>");
+     
+     //          Question=01
+     document.write('<h3> "Question#01 " </h3>');
+
+     document.write("Result: <br>");
+     var digit = 10;
+     document.write("The value of digit is:" + digit +"<br>");
+     document.write('...................................'+"<br>");
+     document.write("The value of ++digit is:" + ++digit +"<br>");
+     document.write("Now the value of digit is: 11"  +"<br><br>");
+
+     var digit3 = 11;
+     document.write("The value of digit++ is:" + digit3++ +"<br>");
+     document.write("Now the value of digit3 is: 12"  +"<br><br>");
+
+     var digit4 = 12;
+     document.write("The value of --digit4 is:" + --digit4 +"<br>");
+     document.write("Now the value of digit4 is: 11"  +"<br><br>");
+
+     var digit5 = 11;
+     document.write("The value of digit5 is:" + digit5-- +"<br>");
+     document.write("Now the value of digit is: 10" +"<br><br>");
+
+         //       Question=02
+     document.write('<h3> "Question#02 " </h3>');
+     var a = 2, b = 1;
+     var result = --a - --b + ++b + b--;
+     document.write("a is: "+ a +"<br>");
+     document.write("b is: "+ b +"<br>");
+     document.write("Result is: "+ result +"<br>");
+
+         //       Question=03
+     document.write('<h3> "Question#03 " </h3>');
+     var name = prompt("What's your name?");
+     document.write("Hello, " + name + "!");
+
+         //       Question=05
+         document.write('<h3> "Question#05 " </h3>');
+         var num = prompt("Enter a number to see its multiplication table (leave blank for 5)");
+        if (num === "") {
+        num = 5;
+          } else {
+            num = parseInt(num);
+                  }
+document.write("Enter a number to see its multiplication table (leave blank for 5)");
+         document.write("<table border='1'>");
+        for (let i = 1; i <= 10; i++) {
+        document.write("<tr>");
+        document.write("<td>" + num + " x " + i + " = " + num * i + "</td>");
+        document.write("</tr>");
+         }
+         document.write("</table>");  
+
+          //       Question=06
+          document.write('<h3> "Question#06 " </h3>');
+          // a) Taking three subject names from the user and storing them in different variables
+var subject1 = prompt("Enter the name of subject 1: ");
+var subject2 = prompt("Enter the name of subject 2: ");
+var subject3 = prompt("Enter the name of subject 3: ");
+
+// b) Total marks for each subject
+var totalMarks = 100;
+
+// c) Taking obtained marks for first subject from the user
+var marks1 = parseInt(prompt("Enter the marks obtained in " + subject1 + ": "));
+
+// d) Taking obtained marks for remaining 2 subjects from the user
+var marks2 = parseInt(prompt("Enter the marks obtained in " + subject2 + ": "));
+var marks3 = parseInt(prompt("Enter the marks obtained in " + subject3 + ": "));
+
+// e) Calculating total marks and percentage
+var totalObtainedMarks = marks1 + marks2 + marks3;
+var percentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
+
+// Showing the result in a table
+document.write("<table border='1'>");
+document.write("<tr><th>Subject</th><th>Total Marks</th><th>Marks Obtained</th></tr>");
+document.write("<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + marks1 + "</td></tr>");
+document.write("<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + marks2 + "</td></tr>");
+document.write("<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + marks3 + "</td></tr>");
+document.write("<tr><td>Total</td><td>" + (totalMarks * 3) + "</td><td>" + totalObtainedMarks + "</td></tr>");
+document.write("<tr><td>Percentage</td><td colspan='2'>" + percentage + "%</td></tr>");
+document.write("</table>");
+
+     
+ 
